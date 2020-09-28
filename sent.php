e66a36fe63c579397bc61254946e4e78262e09cf
Kosuke2000 <69829047+Kosuke2000@users.noreply.github.com>
2020-09-28 11:50:12 +0900

    Create sent.php

---
+++ sent.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>送信内容</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <?php echo $_POST['estimate']; ?>
+    <?php echo $_POST['feeling']; ?>
+    
+</body>
+</html>
